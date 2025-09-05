7159ff65c65972bf677eed59b419e2b55306c121
OnatArslan <onatarslan13@gmail.com>
2025-09-05 20:10:08 +0300

    fix: simplify '/test' route handler by removing unused next parameter

--- src/modules/auth/auth.routes.mjs
+++ src/modules/auth/auth.routes.mjs
@@ -2,6 +2,6 @@ import { Router } from 'express';
 
 export const router = Router();
 
-router.get('/test', (req, res, next) => {
+router.get('/test', (req, res) => {
   res.status(200).json({ status: 'ok' });
 });
