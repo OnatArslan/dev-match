b3a2ccb73582eab404f776143a0355f0cd56e416
OnatArslan <onatarslan13@gmail.com>
2025-10-08 20:16:31 +0300

    commit for health check on auth module and routes (fake controller for testing purpose)

--- src/modules/auth/auth.routes.mjs
+++ src/modules/auth/auth.routes.mjs
@@ -1,3 +1,9 @@
 import { Router } from 'express';
 
 export const router = Router();
+
+router.get(`/hello`, (req, res, next) => {
+  res.status(200).json({
+    status: `success at auth/hello test`,
+  });
+});
